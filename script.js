070ab0305a244d39b69fc0b8ae4416da4600f4e6
Rebecca Johnson <beccanjohnson@gmail.com>
2020-12-13 10:16:21 -0600

    work on assignment 5

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function() {
+   const form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotNameInput = document.getElementById("pilotName");
+      let copilotNameInput = document.getElementById("copilotName");
+      let fuelLevelInput = document.getElementById("fuelLevel");
+      let cargoMassInput = document.getElementById("cargoMass");
+      if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+         alert("All fields are required!");
+         event.preventDefault();
+      } else if (typeof pilotNameInput.vale !== "string") {
+         alert("Pilot Name is invalid!")
+         event.preventDefault();
+      };
+   });
+});
+
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
